99e5ea8d713e61c97af4a85a6a54eb3a91b62f0b
Hector Chamorro <hchamorro96@gmail.com>
2020-07-02 01:28:29 -0500

    Repositorio de MAterialize

---
+++ materialize/js/misfunciones.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".dropdown-trigger").dropdown();
+
+    $('.carousel').carousel();
+    $('.slider').slider();
+    $('.collapsible').collapsible();
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('.tabs').tabs();
+    $('.materialboxed').materialbox();
+
+});
